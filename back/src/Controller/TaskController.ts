3ba18dd6a191d0057baa06313775cb39d86698c6
isaac545454 <isaac.newton1221@gmail.com>
2023-02-12 08:05:00 -0300

    feat: task crotoller create

---
+++ back/src/Controller/TaskController.ts
@@ -0,0 +1,18 @@
+const taskModel = require("../Model/TaskModal");
+import { Request, Response } from "express";
+
+class TaskController {
+  async create(req: Request, res: Response) {
+    const task = new taskModel(req.body);
+    await task
+      .save()
+      .then((response: Response) => {
+        return res.status(201).json(response);
+      })
+      .catch((error: Error) => {
+        return res.status(500).json(error);
+      });
+  }
+}
+
+export default new TaskController();
